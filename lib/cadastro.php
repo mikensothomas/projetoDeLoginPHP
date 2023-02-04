9b919f6441b9d77db9edb030c838bc1e484adab2
mikensothomas <thomasmikenson40@gmail.com>
2023-02-03 21:05:00 -0300

    c

--- lib/cadastro.php
+++ lib/cadastro.php
@@ -1,3 +1,39 @@
+<?php
+if (count($_POST) > 0) {
+
+    include("conexao.php");
+
+    $erro = false;
+    $nomeCompleto = $_POST['nome'];
+    $email = $_POST['email'];
+    $senha = $_POST['senha'];
+
+    if (empty($nomeCompleto)) {
+        $erro = "Escreva teu nome completo no campo nome";
+    }
+
+    if (empty($email)) {
+        $erro = "Escreva teu email";
+    }
+
+    if (empty($senha)) {
+        $erro = "Escreva tua senha";
+    }
+
+    if ($erro) {
+        echo "<p><b>ERRO:$erro</b></p>";
+    } else {
+        $sqli_code = "INSERT INTO login(nomeCompleto, email, senha)
+        VALUES ('$nomeCompleto', '$email', '$senha')";
+        $deu_certo = $mysqli->query($sqli_code) or die($mysqli->error);
+
+        if (!$deu_certo) {
+            echo "<p><b> Erro ao cadastra o cliente </b></p>";
+        }
+    }
+}
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -13,11 +49,11 @@
     <form class="forme" action="login.php" method="POST">
         <h2 class="titulo">Página de cadastro</h2>
         <label class="nome">Nome completo</label><br>
-        <input class="input" type="text" name="nome" placeholder="Digite seu Nome completp"><br><br>
+        <input required class="input" type="text" name="nome" placeholder="Digite seu Nome completp"><br><br>
         <label class="nome">E-mail</label><br>
-        <input class="input" type="text" name="email" placeholder="Digite seu email"><br><br>
+        <input required class="input" type="text" name="email" placeholder="Digite seu email"><br><br>
         <label class="nome">Senha</label><br>
-        <input class="input" type="text" name="senha" placeholder="Digite sua senha"><br><br>
+        <input required class="input" type="text" name="senha" placeholder="Digite sua senha"><br><br>
         <button class="button" type="submit">Cadastra</button>
     </form>
 </body>
