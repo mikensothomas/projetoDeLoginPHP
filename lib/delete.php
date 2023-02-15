ff8272e8cf2371da4ee4315a1a1f68a7063c590e
mikensothomas <thomasmikenson40@gmail.com>
2023-02-15 01:51:15 -0300

    c

--- lib/delete.php
+++ lib/delete.php
@@ -1,17 +1,35 @@
 <?php
+if (isset($_POST['confirmar'])) {
 
-if (isset($_GET['id'])) {
+    include("conexao.php");
+    $id = intval($_GET['id']);
+    $sql_code = "DELETE FROM usuarios WHERE id = '$id'";
+    $sql_query = $mysqli->query($sql_code) or die($mysqli->error);
 
-    include('conexao.php');
-
-    $id = $_GET['id'];
-
-    $sql = "SELECT * FROM usuarios WHERE id = $id";
-
-    $sqlimp = $mysqli->query($sql);
-
-    if ($sqlimp->num_rows > 0) {
-        $sqldelete = "DELETE FROM usuarios WHERE id = $id";
-        $sqlveri = $mysqli->query($sqldelete);
+    if ($sql_query) { ?>
+<h1>Cliente deletado com sucesso!</h1>
+<p><a href="clientes.php">Clique aqui</a> para voltar para a lista de clientes.</p>
+<?php
+        die();
     }
 }
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Deletar Cliente</title>
+</head>
+
+<body>
+    <h1>Tem certeza que deseja deletar este cliente?</h1>
+
+    <form action="" method="post">
+        <a style="margin-right:40px;" href="clientes.php">Não</a>
+        <button name="confirmar" value="1" type="submit">Sim</button>
+    </form>
+</body>
+
+</html>
