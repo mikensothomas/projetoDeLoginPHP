f6d6b061cbb60cbbd51942c7b4d3e6869a057959
mikensothomas <thomasmikenson40@gmail.com>
2023-02-18 17:29:16 -0300

    Master

--- lib/acesso.php
+++ lib/acesso.php
@@ -8,6 +8,7 @@ if (!isset($_SESSION['email']) == true and !isset($_SESSION['senah']) == true) {
 } else {
     $logado = $_SESSION['email'];
 }
+/*gdouivcuwhdçiuweqgdusbduhdbdsdhiHBDUÇbduçsgdsabdçadbUÇDSBDçadvui*/
 ?>
 <!DOCTYPE html>
 <html lang="en">
